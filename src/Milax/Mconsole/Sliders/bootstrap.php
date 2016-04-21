d6594ef4084bc53658dbb2a6bae714b5fd493290
vladimir-r <rudenko@milax.ru>
2016-04-21 13:07:32 +1100

    Add menu

--- src/Milax/Mconsole/Sliders/bootstrap.php
+++ src/Milax/Mconsole/Sliders/bootstrap.php
@@ -26,6 +26,41 @@ return [
         Installer::uninstall();
     },
     'init' => function () {
-        // ..
+        app('API')->menu->push('content', 'sliders_all', [
+            'name' => 'All sliders',
+            'translation' => 'sliders.menu.list.name',
+            'url' => 'news',
+            'description' => 'sliders.menu.list.description',
+            'route' => 'mconsole.sliders.index',
+            'visible' => true,
+            'enabled' => true,
+        ]);
+        app('API')->menu->push('content', 'sliders_create', [
+            'name' => 'Create slider',
+            'translation' => 'sliders.menu.create.name',
+            'url' => 'news',
+            'description' => 'sliders.menu.create.description',
+            'route' => 'mconsole.sliders.create',
+            'visible' => false,
+            'enabled' => true,
+        ]);
+        app('API')->menu->push('content', 'sliders_update', [
+            'name' => 'Update slider',
+            'translation' => 'sliders.menu.update.name',
+            'url' => 'news',
+            'description' => 'sliders.menu.update.description',
+            'route' => 'mconsole.sliders.edit',
+            'visible' => false,
+            'enabled' => true,
+        ]);
+        app('API')->menu->push('content', 'sliders_delete', [
+            'name' => 'Delete slider',
+            'translation' => 'sliders.menu.delete.name',
+            'url' => 'news',
+            'description' => 'sliders.menu.delete.description',
+            'route' => 'mconsole.sliders.destroy',
+            'visible' => false,
+            'enabled' => true,
+        ]);
     },
 ];
